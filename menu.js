9ea6d4ca7770c94c30bb8fc662e140b3447690dd
Maxime Diop <maxime.diopkaypaghian@gmail.com>
2019-05-28 01:23:28 +0200

    responsive

--- menu.js
+++ menu.js
@@ -1,3 +1,11 @@
+
+
+// $(".li1").click(function() {
+//   $(".liste-active").removeClass("liste-active");
+// });
+
+// emil boutton
+
 $("#emil").click(function() {
   $(".liste").toggleClass("liste-active");
   $(".li1").toggleClass("li1-active");
@@ -5,20 +13,28 @@ $("#emil").click(function() {
   $(".Emilia").toggleClass("Emilia-active");
 });
 
+// Passion animation bulle 1
+
 $(".passion_img").hover(function() {
   $(".text_pass").toggleClass("text_pass-active");
   $(".h2_anime").toggleClass("h2_anime-active");
 });
+
+// Passion animation bulle 2
 
 $(".passion_img2").hover(function() {
   $(".text_pass2").toggleClass("text_pass2-active");
   $(".h2_steam").toggleClass("h2_steam-active");
 });
 
+// Passion animation bulle 3
+
 $(".passion_img3").hover(function() {
   $(".text_pass3").toggleClass("text_pass3-active");
   $(".h2_musique").toggleClass("h2_musique-active");
 });
+
+// Nigger theme
 
 $("#switch").on("click", () => {
   if ($("#switch").prop("checked")) {
@@ -28,7 +44,6 @@ $("#switch").on("click", () => {
   } else {
     $(".mybody").removeClass("dark");
     $(".trait").removeClass("trait-dark");
-    localStorage.setItem("theme", " ");
-    localStorage.setItem("checkbox", "");
+
   }
 });
